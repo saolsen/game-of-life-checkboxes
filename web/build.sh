3fc957a659df8957bcee047a37cae0f53209ef96
steve olsen <dev@steve.computer>
2021-10-16 21:12:21 -0500

    web build

---
+++ web/build.sh
@@ -0,0 +1,14 @@
+#! usr/bin/env bash
+
+emcc \
+    -Os \
+    --shell-file shell.html \
+    -DSOKOL_GLES2 \
+    -I../lib \
+    ../plant_game.cpp \
+    ../lib/imgui.cpp \
+    ../lib/imgui_demo.cpp \
+    ../lib/imgui_draw.cpp \
+    ../lib/imgui_tables.cpp \
+    ../lib/imgui_widgets.cpp \
+    -o build/index.html
